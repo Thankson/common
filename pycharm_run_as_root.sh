908c0783ec22e7be8462cc190a44be4132c6d97c
zengqingwen <790494072@qq.com>
2017-11-30 12:03:05 +0800

    add pycharm_run_as_root.sh

---
+++ pycharm_run_as_root.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+passwd=yourUserPasswd
+
+echo $passwd | sudo -S /home/zengqingwen/桌面/IntelliJIDEALicenseServer_linux_amd64
+
+echo $passwd | sudo -S /home/zengqingwen/下载/pycharm-2017.2.4/bin/pycharm.sh
